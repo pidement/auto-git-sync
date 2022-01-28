40c0827b52ea3bc462b1471d9be49a92ecd4e46e
Pablo Vannini <vanninipablo@gmail.com>
2022-01-27 22:54:25 -0300

    debug

--- tests/unit/test_git_check.py
+++ tests/unit/test_git_check.py
@@ -11,7 +11,7 @@ class TestGitCheck(unittest.TestCase):
 
     def setUp(self):
         self.original_path = os.environ["PATH"]
-        self.cmd = [os.getcwd() + IS_UPDATED_SH]
+        self.cmd = ['bash' + os.getcwd() + IS_UPDATED_SH]
 
         process = subprocess.Popen(['pwd'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = process.communicate()
@@ -21,7 +21,7 @@ class TestGitCheck(unittest.TestCase):
         out, err = process.communicate()
         print(out.decode('utf-8').rstrip('\n'))
         print(err.decode('utf-8').rstrip('\n'))
-    
+
     def tearDown(self):
         os.environ["PATH"] = self.original_path
 
